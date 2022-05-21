48255ed858b8afb1ca073a476ce07329282f84c1
yeongjoon.hong <yeongjoon.hong@webtoonscorp.com>
2022-05-21 15:55:52 +0900

    변수이름바꾸기

--- 챕터1/statement03_변수이름바꾸기.js
+++ 챕터1/statement03_변수이름바꾸기.js
@@ -6,6 +6,30 @@ function statement(invoice, plays) {
         { style: "currency", currency: "USD",
             minimumFractionDigits: 2 }).format;
 
+    // perf -> aPerformance
+    const amountFor = (aPerformance, play) => {
+        // thisAmount -> result
+        let result = 0;
+        switch (play.type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르:  ${play.type}`);
+        }
+        return result;
+    };
+
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
         let thisAmount = amountFor(perf, play)
@@ -21,28 +45,4 @@ function statement(invoice, plays) {
     result += `총액: ${format(totalAmount/100)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
-}
-
-function amountFor(aPerformance, play) {
-    let result = 0;
-
-    switch (play.type) {
-        case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-
-        case "comedy":
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
-    return result; // 함수 안에서 값이 바뀌는 변수 반환
 }
